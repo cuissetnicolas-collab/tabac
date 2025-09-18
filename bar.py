312c828e9b3a17e61f8bd70764524a7d22f13047
cuissetnicolas-collab <cuisset.nicolas@gmail.com>
2025-09-18 15:29:36 +0200

    Update bar.py

--- bar.py
+++ bar.py
@@ -34,7 +34,7 @@ if "username" not in st.session_state:
 
 USERS = {
     "aurore": {"password": "12345", "name": "Aurore Demoulin"},
-    "nicolas": {"password": "12345", "name": "Nicolas Cuisset"}
+    "nicolas": {"password": "12345", "name": "Nicolas Cuisset"},
     "manana": {"password": "46789", "name": "Nicolas Cuisset"}
 }
 
